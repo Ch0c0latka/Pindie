bbf427533e6f845afa07409afa386ab7d3319312
Ch0c0latka <149702725+Ch0c0latka@users.noreply.github.com>
2024-05-30 21:53:45 +0300

    <сообщение коммита>

--- app/api/config.js
+++ app/api/config.js
@@ -1,7 +1,7 @@
 export const BASE_URL = 'https://pindie-project.nomorepartiesco.ru'
 
 export const endpoints = {
-  games: `${BASE_URL}/api/games`,
+  games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
   me: `${BASE_URL}/me`,
 }
